f6fb9b8714419733307e71d81b9a223681f2d38b
Timur Gergiya <gergia@gmail.com>
2016-04-25 20:13:19 +0300

    Second assinment

---
+++ hello.py
@@ -0,0 +1,4 @@
+def application(env, start_response):
+	body = '\n'.join(env['QUERY_STRING'].split('&'))
+	start_response('200 OK', [('Content-Type', 'text/html')])
+	return [body]
--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
+# Set up for nginx
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+
+# Set up for gunicorn
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
+sudo /etc/init.d/gunicorn restart
