2acaa0785bc7730d0cae8bb8c3b0ee06159ed016
Timur Gergiya <gergia@gmail.com>
2016-05-19 13:31:28 +0300

    Fixed content-type

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def application(env, start_response):
 	body = '\n'.join(env['QUERY_STRING'].split('&'))
-	start_response('200 OK', [('Content-Type', 'text/html')])
+	start_response('200 OK', [('Content-Type', 'text/plain')])
 	return [body]
