b04bd3e83919778dabd37c0368802293e11fbafc
Timur Gergiya <gergia@gmail.com>
2016-05-20 13:37:01 +0300

    Fixed a bug

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^([0-9]+)$', views.question),
+    url(r'^$', views.okay),
 ]
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.http import Http404
 
+
 # Create your views here.
 def question(request, q_id):
 	return HttpResponse('OK {}'.format(q_id))
+
+
+def okay(request):
+	return HttpResponse('OK')
